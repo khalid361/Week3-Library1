223a68a04070f6ef2d987a30d0c9b29c3e92622c
khalid361 <kalbalushi06@gmail.com>
2025-10-18 16:25:38 +0300

    Added a consturctor to the book class

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Library
+﻿class Book
 {
-    internal class Program
+    string Title;
+    string Author;
+    string ISBN;
+
+    public Book(string bookTitle, string bookAuthor,string bookISBN)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+
+    }
+
+    static void Main(string[] args)
+    {
+        //Create a new instance (object)of the Book class
+        //note how the object name differs from the class name 
+        Book book = new Book();
+
+        //this information id for one book in our library
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        //The is another book object.
+        //note that the object name is 'book1' and not 'book'
+        Book book1 = new Book();
+        book1.Title = "methods";
+        book1.Author = "KHALID";
+        book1.ISBN = "1253643987";
+
+        
+        //output book information to console 
+        book.DisplayInfo();
+        book1.DisplayInfo();
+
     }
 }
